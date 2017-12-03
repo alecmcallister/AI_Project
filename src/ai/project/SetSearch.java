38fa6e7a5d4f097096f844f2da6ce78cd345858a
Alec McAllister <AlecMcAllister@AMP.local>
2017-12-03 14:17:39 -0700

    Still very minor changes to SetSearch.

--- src/ai/project/SetSearch.java
+++ src/ai/project/SetSearch.java
@@ -1,18 +1,28 @@
 package ai.project;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SetSearch
 {
 	ArrayList<Assignments> F;
+	Department department;
 	
-	public SetSearch()
+	public SetSearch(Department department)
 	{
-		
+		this.department = department;
 	}
 	
 	public void DoTheSearchAlready(Assignments a, Assignments b)
 	{
-		Assignments partial;
+		ArrayList<SlotItem> evolutionList =  new ArrayList<>();
+		evolutionList.addAll(department.getAllCourses());
+		
+		ArrayList<SlotItem> unassigned = new ArrayList<>();
+		
+		while (!evolutionList.isEmpty())
+		{
+			SlotItem random = evolutionList.remove(new Random().nextInt(evolutionList.size()));
+		}
 	}
 }
